af656565752a031a2de5ab30020a8f3415942bbe
Rasmus <rasmus.gadensgaard@gmail.com>
2014-09-18 13:04:47 +0200

    Fixed to return entire appliance object as JSON

--- view/applianceView.php
+++ view/applianceView.php
@@ -2,5 +2,5 @@
 require_once "controller/ApplianceController.class.php";
 $applianceController = new ApplianceController($link);
 
-echo json_encode($applianceController->getAppliance($f3->get('PARAMS.id'), $f3->get('PARAMS.lang'))->name);
+echo json_encode($applianceController->getAppliance($f3->get('PARAMS.id'), $f3->get('PARAMS.lang')));
 ?>
