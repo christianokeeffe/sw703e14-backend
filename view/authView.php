50ce6aff0454141a5e1478767dfd5348b7999089
Rasmus <rasmus.gadensgaard@gmail.com>
2014-09-23 11:32:04 +0200

    minor cleanup of code

--- view/authView.php
+++ view/authView.php
@@ -1,10 +1,6 @@
 <?php
-
 require_once "controller/AuthController.php";
-
-
 $authController = new ApiAuth($f3);
-
 $json_decoded = json_decode($f3->get('BODY'), true);
 
 if($authController->auth($f3->get('BODY'), false))
