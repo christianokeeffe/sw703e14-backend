cf8835971e6ed60b97cf8b7449700577ca5e1749
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-10-07 11:47:02 +0200

    GET appliances works!

--- model/ApplianceModel.class.php
+++ model/ApplianceModel.class.php
@@ -17,30 +17,23 @@ class ApplianceModel {
 
 function getAppliances($userID, $lang){
 		$results = $this->db->exec(
-        "SELECT appliances.id, $lang, appliances.price, appliances.energyLabel, appliances.energyConsumption, appliance_type.type
-		FROM appliances
-		INNER JOIN user_appliances
-			ON appliances.id = user_appliances.userID AND user_appliances.userID = $userID
-		INNER JOIN translation 
-			ON translation.id = appliances.name
-		INNER JOIN appliance_type 
-			ON appliances.type = appliance_type.typeID");
+        "SELECT appliances.id, name.$lang AS name, type.$lang AS type, appliances.energyConsumption, appliances.energyLabel, appliances.price
+    FROM appliances
+    INNER JOIN user_appliances
+    ON appliances.id=user_appliances.applianceID  AND user_appliances.userID = $userID
+    INNER JOIN translation AS name
+    ON name.id = appliances.name
+    INNER JOIN appliance_type
+    ON appliances.type = appliance_type.typeID
+    INNER JOIN translation AS type
+    ON type.id = appliance_type.type;");
 		
 		$appliances = array();
         
         foreach($results as $result)
         {
-            $appliances[count($appliances)] = new Appliance($result["id"], $result[$lang], $result["price"], $result["energyLabel"], $result["energyConsumption"]. $result["type"]);
+            $appliances[count($appliances)] = new Appliance($result["id"], $result["name"], $result["price"], $result["energyLabel"], $result["energyConsumption"], $result["type"]);
         }
-		
-        $appliances[0]= "SELECT appliances.id, $lang, appliances.price, appliances.energyLabel, appliances.energyConsumption, appliance_type.type
-        FROM appliances
-        INNER JOIN user_appliances
-            ON appliances.id = user_appliances.userID AND user_appliances.userID = $userID
-        INNER JOIN translation 
-            ON translation.id = appliances.name
-        INNER JOIN appliance_type 
-            ON appliances.type = appliance_type.typeID";
 		return $appliances;
 	}
 } 
