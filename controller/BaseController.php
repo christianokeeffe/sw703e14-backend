38443bc73927e0889e03aff0c56d1401844dc614
Rasmus <rasmus.gadensgaard@gmail.com>
2014-10-10 12:39:49 +0200

    added error messages if debug is enabled

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -18,57 +18,43 @@ class BaseController {
     function beforeroutePOST($f3) {
 
         $authController = new ApiAuth($f3);
-
         $request = $f3->get('BODY');
-
         $isvalid = $authController->auth($request);
 
-        switch ($isvalid) {
-            case '0':
-                if($f3->get('DEBUG') == 2)
-                {
-                    echo "request: ";
-                    var_dump($request);
-                    echo "<br />";
-                }
-                header("HTTP/1.0 401 Unauthorized");
-                echo "ERROR: 401 Unauthorized";
-                die();
-                break;
-            case '419':
-                header("HTTP/1.0 419 Expired");
-                echo "ERROR: 419 Expired";
-                die();
-                break;
-            case '400':
-                header("HTTP/1.0 400 Bad Request");
-                echo "ERROR: 400 Bad Request";
-                die();
-                break;
-            default:
-                header("HTTP/1.1 200 OK");
-        }
+        $this->makeResponse($isvalid, $request, $f3);
     }
 
     function beforerouteGET($f3) {
-
         $authController = new ApiAuth($f3);
-
         $session = $f3->get('PARAMS.session');
         $isvalid = $authController->isValidSession($session);
 
+        $this->makeResponse($isvalid, $f3->get('REQUEST'), $f3);
+    }
 
-        switch ($isvalid) {
+    function makeResponse($statusCode, $req, $f3)
+    {
+        switch ($statusCode) {
             case '0':
                 header("HTTP/1.0 401 Unauthorized");
                 echo "ERROR: 401 Unauthorized";
+                if($f3->get('DEBUG') == 2)
+                {
+                    echo "<br />request:<br />";
+                    var_dump($req);
+                }
                 die();
-            break;
+                break;
             case '419':
                 header("HTTP/1.0 419 Expired");
                 echo "ERROR: 419 Expired";
+                if($f3->get('DEBUG') == 2)
+                {
+                    echo "<br />request:<br />";
+                    var_dump($req);
+                }
                 die();
-            break;
+                break;
             case '400':
                 header("HTTP/1.0 400 Bad Request");
                 echo "ERROR: 400 Bad Request";
