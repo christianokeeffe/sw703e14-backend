92c654951b10a1214eac18e88a76e4586a2790cf
Aleksander <aleksandersn@gmail.com>
2014-10-10 14:35:06 +0200

    changed so type is returned as ID

--- model/ApplianceModel.class.php
+++ model/ApplianceModel.class.php
@@ -17,16 +17,14 @@ class ApplianceModel {
 
 function getAppliances($userID, $lang){
 		$results = $this->db->exec(
-        "SELECT appliances.id, name.$lang AS name, type.$lang AS type, appliances.energyConsumption, appliances.energyLabel, appliances.price
+        "SELECT appliances.id, name.$lang AS name, appliances.type AS type, appliances.energyConsumption, appliances.energyLabel, appliances.price
     FROM appliances
     INNER JOIN user_appliances
     ON appliances.id=user_appliances.applianceID  AND user_appliances.userID = $userID
     INNER JOIN translation AS name
     ON name.id = appliances.name
     INNER JOIN appliance_type
-    ON appliances.type = appliance_type.typeID
-    INNER JOIN translation AS type
-    ON type.id = appliance_type.type;");
+    ON appliances.type = appliance_type.typeID;");
 		
 		$appliances = array();
         
