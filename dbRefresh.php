78b218b4dd274dc18644c583b6aaaa14b5f39ea0
Rasmus <rasmus.gadensgaard@gmail.com>
2014-10-10 11:45:33 +0200

    added dbrefresh

---
+++ dbRefresh.php
@@ -0,0 +1,36 @@
+<?php
+$location = "smartgrid.sql";
+
+$dbhost = "localhost";
+$dbuser = "smartuser";
+$dbpass = "1234";
+$dbname = "smartgrid";
+
+$mysqli = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+if($mysqli->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+$sql = <<<SQL
+    select * from information_schema.tables where TABLE_SCHEMA = '$dbname'
+
+SQL;
+
+if(!$result = $mysqli->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+while($row = $result->fetch_assoc()){
+    $table_name = $row["TABLE_NAME"];
+    $mysqli->query("DROP TABLE " . $table_name);
+    echo "dropped " . $table_name . "<br />";
+}
+
+echo "inserting file";
+
+$commands = file_get_contents($location);
+
+$mysqli->multi_query($commands);
+
+?>
