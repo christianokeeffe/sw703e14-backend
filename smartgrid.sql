316fdc3edd34899ceae78732c1853d2b9ffa3e23
Aleksander <aleksandersn@gmail.com>
2014-10-13 11:57:10 +0200

    added update value to database and api result

--- smartgrid.sql
+++ smartgrid.sql
@@ -251,19 +251,6 @@ CREATE TABLE IF NOT EXISTS `market_price` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `market_price`
---
-
-CREATE TABLE IF NOT EXISTS `market_price` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `time` bigint(20) NOT NULL,
-  `price` double NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
