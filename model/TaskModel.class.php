7713b12290a343ee810a3f42dea9d2c6a8fb559b
Aleksander <aleksandersn@gmail.com>
2014-10-13 11:57:10 +0200

    added update value to database and api result

--- model/TaskModel.class.php
+++ model/TaskModel.class.php
@@ -11,12 +11,12 @@ class TaskModel {
 
     function getTasksByApplianceId($id, $lang)
     {
-        $results = $this->db->exec("SELECT tasks.id, translation.$lang as name, tasks.executionTime, tasks.refAppliance FROM tasks INNER JOIN translation ON tasks.name = translation.id WHERE tasks.refappliance = $id");
+        $results = $this->db->exec("SELECT tasks.id, translation.$lang as name, tasks.executionTime, tasks.refAppliance, tasks.updateValue FROM tasks INNER JOIN translation ON tasks.name = translation.id WHERE tasks.refappliance = $id");
 
         $tasks = array();
         foreach($results as $result)
         {
-            $tasks[count($tasks)] = new Task($result["id"], $result["name"], $result["executionTime"], $result["refAppliance"]);
+            $tasks[count($tasks)] = new Task($result["id"], $result["name"], $result["executionTime"], $result["refAppliance"], $result["updateValue"]);
         }
 
         return $tasks;
