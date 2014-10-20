219d3c7916b4c032dbaafba0169e9879f33393ed
Christian OKeeffe <christian@joedal.dk>
2014-10-20 12:59:37 +0200

    added authenticatino to market price

--- routes_marketprice.php
+++ routes_marketprice.php
@@ -1,8 +1,8 @@
 <?php
 $f3->route('GET /marketprice/@fromtime/@totime/@lang/@session',
     function($f3) {
-        //$auth = new BaseController($f3);
-        //$auth->beforerouteGET($f3);
+        $auth = new BaseController($f3);
+        $auth->beforerouteGET($f3);
         include "view/marketpriceRangeView.php";
     }
 );
