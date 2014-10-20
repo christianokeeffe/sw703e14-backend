1e5dc7dd225acdfa60aff8a67dcc38358fd65104
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-10-20 13:31:54 +0200

    Den ser fin ud nu

--- model/GamedataModel.class.php
+++ model/GamedataModel.class.php
@@ -18,8 +18,7 @@ class GamedataModel {
 	
 	function getUserByID($userID)
     {
-        $result = $this->db->exec(
-            "SELECT * FROM gamedata WHERE userID =  $userID ORDER BY date DESC");
+        $result = $this->db->exec("SELECT * FROM gamedata WHERE userID =  $userID ORDER BY date DESC");
         
         if(count($result) <= 0)
         {
