85ece86e65928043ba8de4fd126165e10a447e36
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-10-24 11:34:44 +0200

    Added save and load status

--- model/metadata/Game.class.php
+++ model/metadata/Game.class.php
@@ -5,15 +5,21 @@ class Game {
     var $savings;
     var $score;
     var $date;
+    var $dishes;
+    var $laundry;
+    var $hygiene;
 
 
-    function __construct($saveID, $userID, $date, $savings, $score)
+    function __construct($saveID, $userID, $date, $savings, $score, $dishes, $laundry, $hygiene)
     {
         $this->saveID = $saveID;
         $this->userID = $userID;
         $this->date = $date;
         $this->savings = $savings;
         $this->score = $score;
+        $this->dishes = $dishes;
+        $this->laundry = $laundry;
+        $this->hygiene = $hygiene;
     }
 }
 
