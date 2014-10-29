f00b670155e8705caf9819dfc840b7afcd78b81f
Mette Pedersen <mettemuzen@hotmail.com>
2014-10-29 12:51:15 +0100

    working

--- model/OptionalTaskModel.class.php
+++ model/OptionalTaskModel.class.php
@@ -14,17 +14,21 @@ class OptionalTaskModel {
         $optional_task = array();
         foreach ($results as $result)
         {
-            $optional_task[count($optional_task)] = new DailyTask($result["id"], $result["name"], $result["taskID"], $result["type"], $result["times"],
+            $optional_task[count($optional_task)] = new OptionalTask($result["id"], $result["name"], $result["taskID"], $result["type"], $result["times"],
                 $result["endTime"]);
         }
 
-        return $daily_task;
+        return $optional_task;
     }
 
     function getDailyTasks($lang)
     {
-        $results = $this->db->exec("SELECT daily_task.id, translation.$lang as name, daily_task.taskID, daily_task.deadline, translation.$lang as type, daily_task.times
-            FROM daily_task INNER JOIN translation ON daily_task.name = translation.id INNER JOIN translation ON optional_task.type = translation.id");
+        $results = $this->db->exec("SELECT daily_task.id, name.$lang as name, daily_task.taskID, daily_task.deadline, type.$lang as type, daily_task.times
+            FROM daily_task
+            INNER JOIN translation AS name 
+                ON daily_task.name = translation.id 
+            INNER JOIN translation AS type 
+                ON optional_task.type = translation.id");
 
         return $this->formatResult($results);
     }
