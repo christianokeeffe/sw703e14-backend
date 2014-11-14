8c42d58c675812ecb77d6fe74408e5fe18bb8a0a
Rasmus <rasmus.gadensgaard@gmail.com>
2014-11-14 10:37:58 +0100

    Unit test of appliance routes complete

--- Tests/ApplianceController_test.php
+++ Tests/ApplianceController_test.php
@@ -9,5 +9,33 @@ class TestOfApplianceController extends HelperFunctions {
         $this->assertTrue($appliance->status == 200);
         $this->assertTrue(count($appliance->data) > 0);
     }
+
+    function testGetAnAppliance() {
+        $appliance = parent::callGet("/appliance/1/");
+
+        $this->assertTrue($appliance->status == 200);
+        $this->assertTrue(count($appliance->data) > 0);
+    }
+
+    function testGetAnApplianceWithLang() {
+        $appliance = parent::callGet("/appliance/1/da/");
+
+        $this->assertTrue($appliance->status == 200);
+        $this->assertTrue(count($appliance->data) > 0);
+    }
+
+    function testGetAppliancesByUserid() {
+        $appliance = parent::callGet("/appliances/1/en/");
+
+        $this->assertTrue($appliance->status == 200);
+        $this->assertTrue(count($appliance->data) > 0);
+    }
+
+    function testGetAppliancesByType() {
+        $appliance = parent::callGet("/appliancesType/3/en/");
+
+        $this->assertTrue($appliance->status == 200);
+        $this->assertTrue(count($appliance->data) > 0);
+    }
 }
 ?>
