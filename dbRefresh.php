151a06d2cbcc7a70ea256464656c0c1766619ca9
Rasmus <rasmus.gadensgaard@gmail.com>
2014-11-14 09:46:11 +0100

    ups

--- dbRefresh.php
+++ dbRefresh.php
@@ -17,9 +17,9 @@ ini_set('max_execution_time', 3000); //300 seconds = 5 minutes
 $location = "smartgrid.sql";
 
 $dbhost = "localhost";
-$dbuser = "smartuser";
-$dbpass = "1234";
-$dbname = "smartgrid";
+$dbuser = "okeeffed_sg_user";
+$dbpass = "QAZ^W.AZ;@8W";
+$dbname = "okeeffed_smartgrid";
 
 $mysqli = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 
